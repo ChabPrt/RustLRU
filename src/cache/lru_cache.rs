ac66b974abc2995f25b2a8cd08fe8838f82aa324
ChabPrt <chabas.alexandre@gmail.com>
2024-12-03 15:15:01 +0100

    :sparkles: Cargo doc

--- src/cache/lru_cache.rs
+++ src/cache/lru_cache.rs
@@ -2,24 +2,127 @@ use std::collections::{HashMap, VecDeque};
 use std::hash::Hash;
 use std::fmt;
 
+/// Structure représentant un cache avec une capacité limitée.
+/// Ce cache maintient un ensemble de paires clé-valeur, en conservant l'ordre d'insertion
+/// et en éjectant les éléments les moins utilisés lorsque la capacité est atteinte.
 pub struct Cache<K, V> {
     capacity: usize,
     data: HashMap<K, V>,
     keys_order: VecDeque<K>,
 }
 
-// Définition du trait CacheOperations
+/// Trait qui définit les opérations de base pour un cache.
 pub trait CacheOperations<K, V> {
+    /// Ajoute une paire clé-valeur dans le cache.
+    ///
+    /// Cette fonction ajoute un nouvel élément dans le cache. Si la clé existe déjà, elle est mise à jour.
+    /// Si la capacité maximale du cache est atteinte, l'élément le moins utilisé est éjecté.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à ajouter dans le cache.
+    /// - `value`: La valeur associée à la clé dans le cache.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.get(&"key_a"), Some(&"value_a"));
+    /// ```
     fn put(&mut self, key: K, value: V);
+
+    /// Récupère une valeur à partir de la clé.
+    ///
+    /// Cette fonction renvoie une référence à la valeur associée à la clé donnée,
+    /// si elle existe dans le cache. Si la clé n'est pas présente, la fonction renvoie `None`.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à récupérer.
+    ///
+    /// # Retour
+    ///
+    /// - Si la clé est présente dans le cache, renvoie `Some(&V)`, où `V` est le type de la valeur associée.
+    /// - Si la clé n'est pas présente, renvoie `None`.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.get(&"key_a"), Some(&"value_a"));
+    /// ```
     fn get(&mut self, key: &K) -> Option<&V>;
+
+    /// Supprime une paire clé-valeur du cache.
+    ///
+    /// Cette fonction supprime un élément du cache en fonction de la clé donnée.
+    /// Si la clé est trouvée et supprimée, la fonction renvoie la valeur supprimée,
+    /// sinon elle renvoie `None`.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à supprimer.
+    ///
+    /// # Retour
+    ///
+    /// - Si l'élément a été trouvé et supprimé, renvoie `Some(V)`, où `V` est la valeur associée à la clé.
+    /// - Si l'élément n'existe pas, renvoie `None`.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(3);
+    /// cache.put("key_c", "value_c");
+    /// assert_eq!(cache.remove(&"key_c"), Some("value_c"));
+    /// assert_eq!(cache.get(&"key_c"), None);
+    /// ```
     fn remove(&mut self, key: &K) -> Option<V>;
+
+    /// Vide le cache.
+    ///
+    /// Cette fonction supprime tous les éléments du cache, réinitialisant ainsi son état.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(3);
+    /// cache.put("key_a", "value_a");
+    /// cache.clear();
+    /// assert_eq!(cache.is_empty(), true);
+    /// ```
     fn clear(&mut self);
 }
 
+/// Implémentation de la structure `Cache`.
 impl<K, V> Cache<K, V>
 where K: Hash + Eq + Clone {
-
-    // Constructor
+    /// Crée un nouveau cache avec une capacité donnée.
+    ///
+    /// Cette fonction permet de créer une nouvelle instance de `Cache` en spécifiant la capacité maximale.
+    /// Le cache sera initialement vide.
+    ///
+    /// # Paramètres
+    ///
+    /// - `capacity`: La capacité maximale du cache. Une fois cette capacité atteinte, les éléments les moins utilisés
+    ///   seront éjectés pour laisser place aux nouveaux éléments.
+    ///
+    /// # Retour
+    ///
+    /// - Retourne une nouvelle instance de `Cache` avec la capacité spécifiée.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let cache = Cache::new(3);
+    /// assert_eq!(cache.len(), 0);
+    /// ```
     pub fn new(capacity: usize) -> Cache<K, V> {
         Cache {
             capacity,
@@ -28,7 +131,25 @@ where K: Hash + Eq + Clone {
         }
     }
 
-    // Put
+    /// Ajoute une paire clé-valeur dans le cache.
+    ///
+    /// Cette fonction met à jour ou ajoute un élément dans le cache. Si la clé existe déjà,
+    /// la valeur est mise à jour et sa position dans l'ordre des clés est modifiée.
+    /// Si la capacité maximale du cache est atteinte, l'élément le moins utilisé est supprimé.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à ajouter.
+    /// - `value`: La valeur à associer à la clé dans le cache.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.get(&"key_a"), Some(&"value_a"));
+    /// ```
     pub fn put(&mut self, key: K, value: V) {
         if self.data.contains_key(&key) {
             self.keys_order.retain(|k| k != &key);
@@ -42,7 +163,24 @@ where K: Hash + Eq + Clone {
         self.keys_order.push_back(key);
     }
 
-    // Get
+    /// Récupère la valeur associée à la clé dans le cache.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à récupérer.
+    ///
+    /// # Retour
+    ///
+    /// - Retourne `Some(&V)` si la clé existe dans le cache, sinon `None`.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.get(&"key_a"), Some(&"value_a"));
+    /// ```
     pub fn get(&mut self, key: &K) -> Option<&V> {
         if self.data.contains_key(key) {
             self.keys_order.retain(|k| k != key);
@@ -52,7 +190,26 @@ where K: Hash + Eq + Clone {
         None
     }
 
-    // Remove
+    /// Supprime une paire clé-valeur du cache.
+    ///
+    /// # Paramètres
+    ///
+    /// - `key`: La clé de l'élément à supprimer.
+    ///
+    /// # Retour
+    ///
+    /// - Si l'élément a été trouvé et supprimé, renvoie la valeur associée à cette clé.
+    ///   Sinon, renvoie `None`.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// cache.remove(&"key_a");
+    /// assert_eq!(cache.get(&"key_a"), None);
+    /// ```
     pub fn remove(&mut self, key: &K) -> Option<V> {
         if self.data.contains_key(key) {
             self.keys_order.retain(|k| k != key);
@@ -62,25 +219,61 @@ where K: Hash + Eq + Clone {
         }
     }
 
-    // Clear
+    /// Vide le cache.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// cache.clear();
+    /// assert_eq!(cache.is_empty(), true);
+    /// ```
     pub fn clear(&mut self) {
         self.data.clear();
         self.keys_order.clear();
     }
 
-    // ---- UTILS ----
-    // Get Size
+    /// Retourne le nombre d'éléments dans le cache.
+    ///
+    /// # Retour
+    ///
+    /// - Retourne un entier représentant le nombre d'éléments dans le cache.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.len(), 1);
+    /// ```
     pub fn len(&self) -> usize {
         self.data.len()
     }
 
-    // IsEmpty ?
+    /// Vérifie si le cache est vide.
+    ///
+    /// # Retour
+    ///
+    /// - Retourne `true` si le cache est vide, sinon `false`.
+    ///
+    /// # Exemple
+    ///
+    /// ```
+    /// use rust_lru::cache::lru_cache::Cache;
+    /// let mut cache = Cache::new(1);
+    /// assert_eq!(cache.is_empty(), true);
+    /// cache.put("key_a", "value_a");
+    /// assert_eq!(cache.is_empty(), false);
+    /// ```
     pub fn is_empty(&self) -> bool {
         self.data.is_empty()
     }
 }
 
-// Implémentation du trait CacheOperations pour Cache
+/// Implémentation du trait `CacheOperations` pour la structure `Cache`.
 impl<K, V> CacheOperations<K, V> for Cache<K, V>
 where K: Hash + Eq + Clone {
     fn put(&mut self, key: K, value: V) {
@@ -100,7 +293,7 @@ where K: Hash + Eq + Clone {
     }
 }
 
-// Display => for each K
+/// Implémentation du trait `fmt::Display` pour afficher le cache.
 impl<K: fmt::Display + Hash + Eq + Clone, V> fmt::Display for Cache<K, V> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         write!(f, "Cache [Capacity: {}, Keys: [", self.capacity)?;
